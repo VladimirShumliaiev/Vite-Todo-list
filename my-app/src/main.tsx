046a7402b854847e111c5164355fbdb8f3b44860
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-06-23 12:25:03 +0300

    add

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { Provider } from 'react-redux'
+// import { Provider } from 'react-redux'
+// import store from './redux/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={stor}>
+    {/* <Provider store={store}> */}
         <App />
-    </Povider>
+    {/* </Provider> */}
   </React.StrictMode>,
 )
