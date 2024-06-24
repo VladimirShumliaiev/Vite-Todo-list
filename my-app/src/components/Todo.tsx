eff07fda68ee06d84e46f66efcc5a7efbda70061
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-06-24 23:14:23 +0300

    add module

---
+++ my-app/src/components/Todo.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './Todo.module.css'
+
+const Todo = () => {
+  return (
+    <div className={style.item}>
+        hello world
+    </div>
+  )
+}
+
+export default Todo
