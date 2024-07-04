d2341429a8e1cb8db4b43d8daf1596c69f3ccb9d
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-07-04 23:26:23 +0300

    Update TodoInput.tsx

--- my-app/src/components/TodoInput.tsx
+++ my-app/src/components/TodoInput.tsx
@@ -29,7 +29,7 @@ const TodoInput: FC<Props> = (props) => {
       value={title} 
       onChange={onChangeHandle}
       ref={inputRef}
-      placeholder='add'
+      placeholder='add todo...'
        />
       <button> ok </button>
     </form>
