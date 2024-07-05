cf0c0d2169cbd8fdb464cbd7c003ebb0ce69b1e9
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-07-05 18:32:20 +0300

    05.07.2024

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,28 +1,11 @@
 
-import { useEffect, useState } from 'react';
 import './App.css'
-import TodoInput from './components/TodoInput';
-import TodoList from './components/TodoList';
-import { addTodo, fetchTodo } from './redux/todoSlice';
-import { useAppDispatch } from './hooks/hooks';
 
 function App() {
-  const [text, setText] = useState('')
-  const dispatch = useAppDispatch()
-
- useEffect(() => {  
-      dispatch(fetchTodo())
- }, [dispatch])
-
-   const addTask = () => {
-      dispatch(addTodo(text))
- }
 
   return (
     <div className='App'>
-        <TodoInput title={text} setTitle={setText} addTodo={addTask}/>
-        <br />
-        <TodoList/>
+          hello
     </div>
   );
 }
