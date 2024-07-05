e1a1154550b8367d8acb6e2a9c36d3520df0a6b4
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-07-05 18:57:05 +0300

    add

--- my-app/src/components/Input.tsx
+++ my-app/src/components/Input.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { FC } from 'react'
 
-const Input = () => {
+type InputProps = {
+    text: string
+    setText: (title: string) => void
+    addTodo: () => void
+}
+
+const Input: FC<InputProps> = (props) => {
+    const {text, setText, addTodo} = props
+
+    const onSubmitHandler: React.FormEventHandler<HTMLFormElement> = (event) => {
+        event.preventDefault()
+
+        if (text.trim().length) {
+            addTodo()
+            setText('')
+        }
+    }
+        
+
+    const onChangeHandler: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+            setText(event.target.value)
+    }
   return (
     <div>
-      
+        <form onSubmit={onSubmitHandler}>
+            <input
+             type="text" 
+             value={text}
+             onChange={onChangeHandler}
+             placeholder='add todo...'
+             />
+            <button> ok </button>
+        </form>
     </div>
   )
 }
--- my-app/src/components/Todo.tsx
+++ my-app/src/components/Todo.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Input from './Input'
+import List from './List'
+import { useAppDispatch } from '../hooks/hooks'
+import { addTodo, fetchTodo } from '../redux/todoSlice'
 
 const Todo = () => {
+    const [title, setTitle] = useState('')
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        dispatch(fetchTodo())
+    },[dispatch])
+
+    const addTask = () => {
+        dispatch(addTodo(title))
+    }
+
   return (
     <div>
-      
+        <Input text={title} setText={setTitle} addTodo={addTask}/>
+        <List/>
     </div>
   )
 }
