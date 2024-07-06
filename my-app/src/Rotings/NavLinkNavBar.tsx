0ed54b5e4d0982cf3e4e732a1c02e2cc34e3eca7
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-07-06 11:30:10 +0300

    add

--- my-app/src/Rotings/NavLinkNavBar.tsx
+++ my-app/src/Rotings/NavLinkNavBar.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Todo from "../components/Todo/Todo";
 
 const NavLinkNavBar = () => {
   return (
     <div>
-      
+      <div>
+        <NavLink to={"/"}> Home </NavLink>
+      </div>
+      <div>
+        <NavLink to={"todo"}> Todo</NavLink>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavLinkNavBar
+export default NavLinkNavBar;
--- my-app/src/components/NavBar/NavBar.tsx
+++ my-app/src/components/NavBar/NavBar.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import NavLinkNavBar from '../../Rotings/NavLinkNavBar'
 
 const NavBar = () => {
   return (
     <div>
-      NavBar
+     <NavLinkNavBar/>
     </div>
   )
 }
