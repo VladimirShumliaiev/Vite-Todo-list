6ce8d4369eafd3dccf88847c65954244d86f6e17
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-07-06 10:37:39 +0300

    add

--- my-app/src/components/Todo/Todo.tsx
+++ my-app/src/components/Todo/Todo.tsx
@@ -1,27 +1,27 @@
-import React, { useEffect, useState } from 'react'
-import Input from './Input'
-import List from './List'
-import { useAppDispatch } from '../../hooks/hooks'
-import { addTodo, fetchTodo } from '../../redux/todoSlice'
+import React, { useEffect, useState } from "react";
+import Input from "./Input";
+import List from "./List";
+import { useAppDispatch } from "../../hooks/hooks";
+import { addTodo, fetchTodo } from "../../redux/todoSlice";
 
 const Todo = () => {
-    const [title, setTitle] = useState('')
-    const dispatch = useAppDispatch()
+  const [title, setTitle] = useState("");
+  const dispatch = useAppDispatch();
 
-    useEffect(() => {
-        dispatch(fetchTodo())
-    },[dispatch])
+  useEffect(() => {
+    dispatch(fetchTodo());
+  }, [dispatch]);
 
-    const addTask = () => {
-        dispatch(addTodo(title))
-    }
+  const addTask = () => {
+    dispatch(addTodo(title));
+  };
 
   return (
     <div>
-        <Input text={title} setText={setTitle} addTodo={addTask}/>
-        <List/>
+      <Input text={title} setText={setTitle} addTodo={addTask} />
+      <List />
     </div>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
