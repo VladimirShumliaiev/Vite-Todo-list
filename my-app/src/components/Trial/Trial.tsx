046b667100462a5783428b87d33ca0305cb20c24
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-07-12 00:32:34 +0300

    add

--- my-app/src/components/Trial/Trial.tsx
+++ my-app/src/components/Trial/Trial.tsx
@@ -1,26 +1,70 @@
 import React, { useState } from "react";
 import { trialObject } from "./trial";
-import './Trial.css'
-
+import "./Trial.css";
 
 const Trial = () => {
-  const [state, setState] = useState(trialObject.one)
+  const [state, setState] = useState(trialObject.one);
 
   const handleOnChange = (value: string) => {
-      setState(value)
-  }
+    setState(value);
+  };
 
   return (
     <div>
-      <span>
-        <button className={state === trialObject.one ? 'trial-active' : ''} onClick={() => handleOnChange(trialObject.one)}>1</button>{' '}
-      </span>
-      <span>
-        <button className={state === trialObject.two ? 'trial-active' : ''} onClick={() => handleOnChange(trialObject.two)}>2</button>{' '}
-      </span>
-      <span>
-        <button className={state === trialObject.three ? 'trial-active' : ''} onClick={() => handleOnChange(trialObject.three)}>3</button>
-      </span>
+      <div>
+        <span>
+          <button
+            className={state === trialObject.one ? "trial-active" : ""}
+            onClick={() => handleOnChange(trialObject.one)}
+          >
+            1
+          </button>{" "}
+        </span>
+        <span>
+          <button
+            className={state === trialObject.two ? "trial-active" : ""}
+            onClick={() => handleOnChange(trialObject.two)}
+          >
+            2
+          </button>{" "}
+        </span>
+        <span>
+          <button
+            className={state === trialObject.three ? "trial-active" : ""}
+            onClick={() => handleOnChange(trialObject.three)}
+          >
+            3
+          </button>
+        </span>
+      </div>
+            <pre/>
+      <div>
+        <span>
+          <button
+            className={state === trialObject.four ? "trial-green" : ""}
+            onClick={() => handleOnChange(trialObject.four)}
+          >
+            4
+          </button>{" "}
+        </span>
+        <span>
+          <button
+            className={state === trialObject.five ? "trial-yellow" : ""}
+            onClick={() => handleOnChange(trialObject.five)}
+          >
+            4
+          </button>{" "}
+        </span>
+        <span>
+          <button
+            className={state === trialObject.six ? "trial-purple" : ""}
+            onClick={() => handleOnChange(trialObject.six)}
+          >
+            4
+          </button>
+        </span>
+      </div>
+
       <div>{state}</div>
     </div>
   );
--- my-app/src/components/Trial/trial.ts
+++ my-app/src/components/Trial/trial.ts
@@ -2,4 +2,7 @@ export const trialObject = {
   one: "hello JS",
   two: "hello TS",
   three: "hello React",
+  four: "hello html",
+  five: "hello css",
+  six: "hello World",
 };
