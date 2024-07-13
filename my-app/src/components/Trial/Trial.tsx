1420c69c29bd7c435b71f0151ecb625949325320
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-07-13 12:16:03 +0300

    Update Trial.tsx

--- my-app/src/components/Trial/Trial.tsx
+++ my-app/src/components/Trial/Trial.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import TrailButton from './TrialButton'
+import { trialObject } from "./trialObject";
 
 const Trial = () => {
-  const [state, setState] = useState('')
+  const [state, setState] = useState(trialObject.one)
   return (
     <div>
     <TrailButton  state={state} setState={setState}/>
