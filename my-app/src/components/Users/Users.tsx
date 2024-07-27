d7cc8e0429c213eec6372ba6a2cd55a87276f4e9
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-07-27 14:13:04 +0300

    Update Users.tsx

--- my-app/src/components/Users/Users.tsx
+++ my-app/src/components/Users/Users.tsx
@@ -1,10 +1,13 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { fetchUser } from '../../redux/usersSlice'
 import { useAppDispatch } from '../../hooks/hooks'
 import UserList from './UserList'
 
 const Users = () => {
     const dispatch = useAppDispatch()
+    const [time, setTime] = useState(new Date())
+
+    setInterval(() => setTime(new Date),1000)
 
     useEffect(() => {
         dispatch(fetchUser())
@@ -12,6 +15,9 @@ const Users = () => {
 
   return (
     <div>
+        {time.toDateString()}
+        {' '}
+        {time.toLocaleTimeString()}
         <UserList />
     </div>
   )
