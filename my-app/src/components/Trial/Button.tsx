d196eaed10217363c308a6b059e8b4565d0b9968
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-08-15 18:25:29 +0300

    add

--- my-app/src/components/Trial/Button.tsx
+++ my-app/src/components/Trial/Button.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { FC } from "react";
+import './Trial.css'
+import { trialObject } from "./trialObject";
 
-const Button = () => {
+type ButtonProps = {
+  state: string;
+  setState: (state: string) => void;
+};
+
+const Button: FC<ButtonProps> = (props) => {
+  const { state, setState } = props;
+
+  const handleOnclick = (value: string) => {
+    setState(value)
+  }
+
   return (
     <div>
-      
+      <div>
+        <button className={state === trialObject.one ? 'trial-blue' : ''} onClick={() => handleOnclick(trialObject.one)}>one</button>
+        <button className={state === trialObject.two ? 'trial-green' : ''} onClick={() => handleOnclick(trialObject.two)}>two</button>
+      </div>
+      <div>
+        <button className={state === trialObject.three ? 'trial-yellow' : ''} onClick={() => handleOnclick(trialObject.three)}>tree</button>
+        <button className={state === trialObject.four ? 'trial-purple' : ''} onClick={() => handleOnclick(trialObject.four)}>four</button>
+        <button className={state === trialObject.five ? 'trial-red ' : ''} onClick={() => handleOnclick(trialObject.five)}>five</button>
+      </div>
+      <div>
+        <button className={state === trialObject.six ? 'trial-skyBlue' : ''}onClick={() => handleOnclick(trialObject.six)}>six</button>
+      </div>
+      {state}
     </div>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
