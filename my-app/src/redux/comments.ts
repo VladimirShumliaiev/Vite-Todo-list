fbdfe8e5c1d5c606c3e95bed43d0b84ef93403d6
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-08-22 20:43:10 +0300

    add

--- my-app/src/redux/comments.ts
+++ my-app/src/redux/comments.ts
@@ -1,4 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 type Comments = {
   postId: string;
@@ -15,10 +16,44 @@ const initialState: CommentsState = {
   commentsList: [],
 };
 
-const comments = createSlice({
+export const fetchComments = createAsyncThunk<
+  Comments[],
+  undefined,
+  { rejectValue: string }
+>("comments/fetchComments", async (_, { rejectWithValue }) => {
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/comments"
+  );
+
+  if (!response) {
+    return rejectWithValue("error fetchComments");
+  }
+
+  return response.data;
+});
+
+export const deleteComments = createAsyncThunk<
+  string,
+  string,
+  { rejectValue: string }
+>("comments/deleteComments", async (id, { rejectWithValue }) => {
+  const response = await axios.delete(
+    `https://jsonplaceholder.typicode.com/comments/${id}`
+  );
+
+  if (!response) {
+    return rejectWithValue("error delete comments");
+  }
+
+  return id;
+});
+
+export const addComments;
+
+const commentsSlice = createSlice({
   name: "comments",
   initialState: initialState,
   reducers: {},
 });
 
-export default comments.reducer;
+export default commentsSlice.reducer;
