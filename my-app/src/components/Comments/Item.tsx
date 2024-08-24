513444c42287f9fca4b15d11cc1b9a6a91e46849
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-08-24 22:33:03 +0300

    Update Item.tsx

--- my-app/src/components/Comments/Item.tsx
+++ my-app/src/components/Comments/Item.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from "react";
-import { addComments, Comments, deleteComments } from "../../redux/commentsSlice";
+import {Comments, deleteComments } from "../../redux/commentsSlice";
 import { useAppDispatch } from "../../hooks/hooks";
-import InputEmail from "./inputEmail";
 
 type Props = Comments;
 
@@ -13,16 +12,12 @@ const Item: FC<Props> = (props) => {
     dispatch(deleteComments(id))
   }
 
-  const handleAdd = () => {
-    dispatch(addComments(id))
-  }
   return (
     <div>
       <div>id: {id}</div>
       <div>email: {email}</div>
       <div>name: {name}</div>
       <div>body: {body}</div>
-      <button onClick={() => handleAdd()}>add</button>
       <button onClick={handleDelete}>delete</button>
       <hr />
     </div>
