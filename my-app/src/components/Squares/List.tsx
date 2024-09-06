f64bda678e755811d4bf8ccfb8fdc2bb27beec9c
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-09-06 16:39:33 +0300

    Update List.tsx

--- my-app/src/components/Squares/List.tsx
+++ my-app/src/components/Squares/List.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { squaresObject } from './squares'
 
 
 const List = () => {
+  
   return (
     <div>
-      
+        {
+          squaresObject.map((e,i) => {})
+        }
     </div>
   )
 }
