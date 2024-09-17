8557f59f0c22658e741e494d5f30eea80ede5932
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-09-17 20:27:26 +0300

    Update trialObject.ts

--- my-app/src/components/Trial/trialObject.ts
+++ my-app/src/components/Trial/trialObject.ts
@@ -1,7 +1,7 @@
 export const trialObject = {
   one: "Hello JS",
   two: "Hello TS",
-  three: "hello React",
+  three: "Hello React",
   four: "hello html",
   five: "hello css",
   six: "hello World!!!",
