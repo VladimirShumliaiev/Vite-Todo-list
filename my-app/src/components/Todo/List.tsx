5fabae227374ef00da5390d0a5e2f98129706169
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-09-20 20:33:53 +0300

    add

--- my-app/src/components/Todo/List.tsx
+++ my-app/src/components/Todo/List.tsx
@@ -1,14 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useAppSelector } from '../../hooks/hooks'
 import ListItem from './ListItem'
+import ReactPaginate from 'react-paginate'
+import style from './Todo.module.css'
 
 const List = () => {
   const list = useAppSelector(state => state.todo.todoList)
+
+  const [currentItems, setCurrentItems] = useState(list);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+  const itemsPerPage = 10
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage;
+    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+    setCurrentItems(list.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(list.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage, list]);
+
+  const handlePageClick = (event: any) => {
+    const newOffset = event.selected * itemsPerPage % list.length;
+    console.log(`User requested page number ${event.selected}, which is offset ${newOffset}`);
+    setItemOffset(newOffset);
+  };
+
   return (
     <div>
         {
-          list.map(todo => <ListItem key={todo.id} {...todo}/>)
+          currentItems.map(todo => <ListItem key={todo.id} {...todo}/>)
         }
+  <ReactPaginate
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={3}
+        marginPagesDisplayed={2}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        pageClassName="page-item"
+        pageLinkClassName={style.pageNum}
+        previousClassName="page-item"
+        previousLinkClassName={style.pageNum}
+        nextClassName="page-item"
+        nextLinkClassName={style.pageNum}
+        breakLabel="..."
+        breakClassName="page-item"
+        breakLinkClassName={style.pageNum}
+        containerClassName={style.Pagination}
+        activeClassName={style.active}
+        activeLinkClassName={style.active}
+        renderOnZeroPageCount={null}
+      />
     </div>
   )
 }
