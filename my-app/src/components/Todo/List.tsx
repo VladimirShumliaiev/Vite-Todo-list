d0b1e59e3d8ec09fbf7e2cd85a2b436d80f4923d
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-10-30 19:35:06 +0200

    add

--- my-app/src/components/Todo/List.tsx
+++ my-app/src/components/Todo/List.tsx
@@ -1,57 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import { useAppSelector } from '../../hooks/hooks'
-import ListItem from './ListItem'
-import ReactPaginate from 'react-paginate'
-import style from './Todo.module.css'
+import React from 'react'
 
 const List = () => {
-  const list = useAppSelector(state => state.todo.todoList)
-
-  const [currentItems, setCurrentItems] = useState(list);
-  const [pageCount, setPageCount] = useState(0);
-  const [itemOffset, setItemOffset] = useState(0);
-  const itemsPerPage = 13
-
-  useEffect(() => {
-    const endOffset = itemOffset + itemsPerPage;
-    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
-    setCurrentItems(list.slice(itemOffset, endOffset));
-    setPageCount(Math.ceil(list.length / itemsPerPage));
-  }, [itemOffset, itemsPerPage, list]);
-
-  const handlePageClick = (event: { selected: number }) => {
-    const newOffset = event.selected * itemsPerPage % list.length;
-    console.log(`User requested page number ${event.selected}, which is offset ${newOffset}`);
-    setItemOffset(newOffset);
-  };
-
   return (
     <div>
-        {
-          currentItems.map(todo => <ListItem key={todo.id} {...todo}/>)
-        }
-        {' '}
-  <ReactPaginate
-        nextLabel="next >"
-        onPageChange={handlePageClick}
-        pageRangeDisplayed={5}
-        marginPagesDisplayed={2}
-        pageCount={pageCount}
-        previousLabel="< previous"
-        pageClassName={style.pageItem}
-        pageLinkClassName={style.pageNum}
-        previousClassName={style.previousClassName}
-        previousLinkClassName={style.pageNum}
-        nextClassName={style.previousClassName}
-        nextLinkClassName={style.pageNum}
-        breakLabel="..."
-        breakClassName={style.pageItem}
-        breakLinkClassName={style.pageNum}
-        containerClassName={style.Pagination}
-        activeClassName={style.active}
-        activeLinkClassName={style.active}
-        renderOnZeroPageCount={null}
-      />
+      
     </div>
   )
 }
--- my-app/src/components/Todo/ListItem.tsx
+++ my-app/src/components/Todo/ListItem.tsx
@@ -1,33 +1,9 @@
-import React, { FC } from 'react'
-import { useAppDispatch } from '../../hooks/hooks'
-import { completeTodo, deleteTodo } from '../../redux/todoSlice'
+import React from 'react'
 
-type ItemProps = {
-  id: string
-  completed: boolean
-  title: string
-}
-
-const ListItem:FC<ItemProps> = (props) => {
-  const {id, completed, title} = props
-  const dispatch = useAppDispatch()
-
-  const handleOnChange = () => {
-    dispatch(completeTodo(id))
-  }
-
-  const handleOnClick = () => {
-    if (window.confirm('delete todo')) {
-      dispatch(deleteTodo(id))
-    }
-
-  }
+const ListItem = () => {
   return (
     <div>
-      <input type={'checkbox'} checked={completed} onChange={handleOnChange} />
-        {title}
-        {' '}
-        <button onClick={handleOnClick}> x </button>
+      
     </div>
   )
 }
