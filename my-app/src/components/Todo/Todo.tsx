4185d907735766e86b1aca90169250095bacb0be
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-10-30 19:36:05 +0200

    Update Todo.tsx

--- my-app/src/components/Todo/Todo.tsx
+++ my-app/src/components/Todo/Todo.tsx
@@ -1,30 +1,12 @@
-import React, { useEffect, useState } from 'react'
-import List from './List'
-import { useAppDispatch, useAppSelector } from '../../hooks/hooks'
-import { addTodo, fetchTodo } from '../../redux/todoSlice'
-import Input from './Input'
+import React from 'react'
 
 const Todo = () => {
-  const [title, setTitle] = useState('')
-  const {error, loading} = useAppSelector(state => state.todo)
-  const dispatch = useAppDispatch()
-
-useEffect(() => {
-  dispatch(fetchTodo())
-},[dispatch])
-
-const addTask = () => {
-  dispatch(addTodo(title))
-}
-console.log('111',error)
   return (
     <div>
-        <Input text={title} setText={setTitle} addTodo={addTask}/>
-        {loading && <h2>Loading...</h2> }
-        {error && <h2>Error:{error}</h2>}
-        <List/>
+      
     </div>
   )
 }
 
 export default Todo
+
