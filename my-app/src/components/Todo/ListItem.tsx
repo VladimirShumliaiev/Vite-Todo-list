044bc6ca847b6ecbe55945da892f2f25821722bc
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-11-04 20:31:34 +0200

    add

--- my-app/src/components/Todo/ListItem.tsx
+++ my-app/src/components/Todo/ListItem.tsx
@@ -3,12 +3,10 @@ import { useAppSelector } from '../../hooks/hooks'
 
 const ListItem = () => {
 
-  const list = useAppSelector(state => state.todo.todoList)
+  
   return (
     <div>
-          {
-            list.map(todo => <)
-          }
+         
     </div>
   )
 }
