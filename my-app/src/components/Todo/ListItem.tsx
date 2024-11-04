0a9598a995e70e968de74903bb94a145d1d52647
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-11-04 20:28:09 +0200

    Update ListItem.tsx

--- my-app/src/components/Todo/ListItem.tsx
+++ my-app/src/components/Todo/ListItem.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useAppSelector } from '../../hooks/hooks'
 
 const ListItem = () => {
+
+  const list = useAppSelector(state => state.todo.todoList)
   return (
     <div>
-      
+        
     </div>
   )
 }
