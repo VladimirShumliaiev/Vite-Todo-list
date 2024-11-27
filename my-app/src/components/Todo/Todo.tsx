654ad123fcd326c5fc48873ae4b9ea8d1a173787
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-11-27 17:44:39 +0200

    add

--- my-app/src/components/Todo/Todo.tsx
+++ my-app/src/components/Todo/Todo.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Input from './Input'
 import List from './List'
+import { useAppDispatch } from '../../hooks/hooks'
+import { addTodo, fetchTodo } from '../../redux/todoSlice'
 
 const Todo = () => {
+  const [state, setState] = useState('')
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(fetchTodo())
+  },[dispatch])
+
+  const addTask = () => {
+    dispatch(addTodo(state))
+  }
   return (
     <div>
-      <Input/>
+      <Input state={state} setSate={setState} addTodo={addTask}/>
       <List/>
     </div>
   )
